53e00c3f054a575a29c8170f68bd6bae812c0f06
Omar Emad <oemad3366@gmail.com>
2026-03-15 18:10:07 +0000

    leet code subsets has been solved

--- LeetCodeSubsets/subsets.cpp
+++ LeetCodeSubsets/subsets.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        res.push_back({});
-        if(nums.size() ==0) return res;
+        vector<vector<int>> res = {{}};  // start with empty set
         
+        for (int num : nums) {
+            int sz = res.size();
+            for (int i = 0; i < sz; ++i) {
+                vector<int> temp = res[i];
+                temp.push_back(num);
+                res.push_back(temp);
+            }
+        }
+        return res;
     }
 };
-
 
 int main(){
     
